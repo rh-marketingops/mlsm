297724701edc208dd13b568e29a52d44cf4d6aec
colemanja91 <colemanja91@gmail.com>
2016-10-19 14:43:51 -0400

    consolidate verbose logic

--- mlsm/mlsm.py
+++ mlsm/mlsm.py
@@ -6,44 +6,27 @@ def RunModelsAll(models, records, summaryModels=[], verbose = False, db = None, 
     returnRecords = []
 
     if verbose:
-
-        for record in tqdm(records):
-
-            record = RunModels(models, record)
-
-            record = RunSummaryModels(summaryModels, record)
-
-            returnRecords.append(record)
-
-            if db and collection and dbIdentifier:
-
-                recordInsert = {}
-
-                recordInsert[dbIdentifier] = record[dbIdentifier]
-
-                recordInsert['results'] = record['results']
-
-                db[collection].insert_one(recordInsert)
-
+        runRecords = tqdm(records)
     else:
+        runRecords = records
 
-        for record in records:
+    for record in runRecords:
 
-            record = RunModels(models, record)
+        record = RunModels(models, record)
 
-            record = RunSummaryModels(summaryModels, record)
+        record = RunSummaryModels(summaryModels, record)
 
-            returnRecords.append(record)
+        returnRecords.append(record)
 
-            if db and collection and dbIdentifier:
+        if db and collection and dbIdentifier:
 
-                recordInsert = {}
+            recordInsert = {}
 
-                recordInsert[dbIdentifier] = record[dbIdentifier]
+            recordInsert[dbIdentifier] = record[dbIdentifier]
 
-                recordInsert['results'] = record['results']
+            recordInsert['results'] = record['results']
 
-                db[collection].insert_one(recordInsert)
+            db[collection].insert_one(recordInsert)
 
     return returnRecords
 
