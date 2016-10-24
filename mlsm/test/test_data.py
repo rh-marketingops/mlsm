4548ddd0a0f17aeb27f2b16a6b408766a0b923ca
colemanja91 <colemanja91@gmail.com>
2016-10-24 14:51:42 -0400

    modified data for new schema, updated existing fcns to use

--- mlsm/test/test_data.py
+++ mlsm/test/test_data.py
@@ -1,23 +1,59 @@
-modelRunAllRecords = [
-    {'id': 1, 'data': {'a': 1, 'b': 2}, 'results': {}},
-    {'id': 2, 'data': {'a': 1, 'b': 2}, 'results': {}},
-    {'id': 3, 'data': {'a': 1, 'b': 2}, 'results': {}}
-]
+# modelRunAllRecords = [
+#     {'id': 1, 'data': {'a': 1, 'b': 2}, 'results': {}},
+#     {'id': 2, 'data': {'a': 1, 'b': 2}, 'results': {}},
+#     {'id': 3, 'data': {'a': 1, 'b': 2}, 'results': {}}
+# ]
+#
+# modelRunAllModelsAllRecords = [
+#     {'id': 1, 'data': {'a': 1, 'b': 2}},
+#     {'id': 2, 'data': {'a': 1, 'b': 2}},
+#     {'id': 3, 'data': {'a': 1, 'b': 2}}
+# ]
+#
+# modelRunSummary = [
+#     {'id': 1, 'data': {'a': 1, 'b': 2}},
+#     {'id': 2, 'data': {'a': 1, 'b': 2}},
+#     {'id': 3, 'data': {'a': 1, 'b': 2}}
+# ]
+#
+# modelRunSummaryOne = [
+#     {'id': 1, 'data': {'a': 1, 'b': 2}}
+# ]
+#
+# modelRunAllModelsOneRecord = {'id': 1, 'data': {'test': {'0.0.0' {'a': 1, 'b': 2}}}}
 
-modelRunAllModelsAllRecords = [
-    {'id': 1, 'data': {'a': 1, 'b': 2}},
-    {'id': 2, 'data': {'a': 1, 'b': 2}},
-    {'id': 3, 'data': {'a': 1, 'b': 2}}
-]
+recordsOne = {'id': 1,
+              'data': {
+                'test1': {'0.0.0': {'a': 1, 'b': 2}},
+                'test2': {'0.0.0': {'a': 1, 'b': 2}},
+                'test3': {'0.0.0': {'a': 1, 'b': 2}},
+                'testSummary': {'0.0.0': {'a': 1, 'b': 2}}
+               },
+               'results': {}}
 
-modelRunSummary = [
-    {'id': 1, 'data': {'a': 1, 'b': 2}},
-    {'id': 2, 'data': {'a': 1, 'b': 2}},
-    {'id': 3, 'data': {'a': 1, 'b': 2}}
+recordsMulti = [
+    {'id': 1,
+                  'data': {
+                    'test1': {'0.0.0': {'a': 1, 'b': 2}},
+                    'test2': {'0.0.0': {'a': 1, 'b': 2}},
+                    'test3': {'0.0.0': {'a': 1, 'b': 2}},
+                    'testSummary': {'0.0.0': {'a': 1, 'b': 2}}
+                   },
+                   'results': {}},
+    {'id': 2,
+                  'data': {
+                    'test1': {'0.0.0': {'a': 1, 'b': 2}},
+                    'test2': {'0.0.0': {'a': 1, 'b': 2}},
+                    'test3': {'0.0.0': {'a': 1, 'b': 2}},
+                    'testSummary': {'0.0.0': {'a': 1, 'b': 2}}
+                   },
+                   'results': {}},
+    {'id': 3,
+                  'data': {
+                    'test1': {'0.0.0': {'a': 1, 'b': 2}},
+                    'test2': {'0.0.0': {'a': 1, 'b': 2}},
+                    'test3': {'0.0.0': {'a': 1, 'b': 2}},
+                    'testSummary': {'0.0.0': {'a': 1, 'b': 2}}
+                   },
+                   'results': {}}
 ]
-
-modelRunSummaryOne = [
-    {'id': 1, 'data': {'a': 1, 'b': 2}}
-]
-
-modelRunAllModelsOneRecord = {'id': 1, 'data': {'a': 1, 'b': 2}}
